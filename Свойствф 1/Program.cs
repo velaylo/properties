73d32705254e68c4a3c73ea5cbc2ad314bee6b3d
yankovich.olga24 <yankovich.olga24@gmail.com>
2019-06-28 17:26:42 +0300

    Добавьте файлы проекта.

---
+++ Свойствф 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Свойства_и_индексаторы_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ (английский): ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Symbol sym = new Symbol();
+
+            sym.Symb = symbol;
+
+            // char correctSymbol = sym.Symb;
+            //Console.WriteLine(correctSymbol);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Symbol
+    {
+        public char symbol_1 = ' ';
+
+        public char Symb
+        {
+            get
+            {
+                return symbol_1;
+            }
+
+            set
+            {
+                if ((int)value <= (int)'Z' && (int)value >= (int)'A')
+                {
+                    symbol_1 = value;
+                    Console.WriteLine($"Введённый символ {symbol_1} попадает в промежуток от А до Z включительно");
+                }
+                else
+                {
+                    Console.WriteLine("Введённый символ не попадает в промежуток от А до Z включительно");
+                }
+            }
+        }
+    }
+}
